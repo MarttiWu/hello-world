d842729c5e193b2894d2c8a4104e1b900fc41b03
MarttiWu <69631800+MarttiWu@users.noreply.github.com>
2020-08-18 15:54:12 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//
+//  Created by WU,MENG-TING on 2020/8/18.
+//  Copyright © 2020 WU,MENG-TING. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Hello World!"<<endl;
+    return 0;
+}
